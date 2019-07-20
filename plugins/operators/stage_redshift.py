055e91f2756c850a505290819f9e96cada6cce3f
Robert Honeybul <honeybulr@gmail.com>
2019-07-20 19:00:24 +0800

    added stage to redshift operator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -8,8 +9,13 @@ class StageToRedshiftOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 #  Example:
+                 #     redshift_conn_id=your-connection-name
+                 s3_file_path,
+                 target_table,
+                 file_type='.csv'
+                 redshift_conn_id='amazon-redshift',
+                 aws_conn_id='amazon-s3',
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
@@ -18,7 +24,18 @@ class StageToRedshiftOperator(BaseOperator):
         # self.conn_id = conn_id
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        # self.log.info('StageToRedshiftOperator not implemented yet')
+
+        sql_statement = '''
+        COPY {}
+        FROM {}
+        WITH (FORMAT {})
+        '''.format(target_table, s3_file_path, file_type)
+
+        aws_hook = AwsHook(aws_conn_id)
+        credentials = aws_hook.get_credentials()
+        redshift_hook = PostgresHook("redshift")
+        redshift_hook.run(sql_statement)
 
 
 
